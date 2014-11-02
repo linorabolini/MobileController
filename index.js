24a075866386a33d40837bff3640fef8b86f71cd
linorabolini <linorabolini@gmail.com>
2014-11-02 20:58:57 -0300

    improved server + clients

--- index.js
+++ index.js
@@ -12,12 +12,22 @@ io.on('connection', function(socket) {
   debug('a user connected');
   socket.emit('connection');
 
-  socket.on('register as server', function() {
-    Server(socket);
+  socket.on('register as server', function(name) {
+
+    var clientRoom = 'clients:' + name;
+    
+    // close other sockets in the clients room
+    var room = findClientsSocket(clientRoom);
+    room.forEach(function(client) {
+        client.disconnect();
+    });
+
+    // create the room
+    Server(socket, name);
   });
 
-  socket.on('register as client', function() {
-    Client(socket);
+  socket.on('register as client', function(name) {
+    Client(socket, name);
   });
 });
 
@@ -25,38 +35,67 @@ http.listen(3000, function() {
   debug('listening on *:3000');
 });
 
-function Server(socket) {
-  debug('a server registered');
-  socket.join('servers');
+function Server(socket, name) {
+  var serverRoom = 'server:' + name;
+  debug('server registered: ' + serverRoom);
+
+  socket.join(serverRoom);
 
   // disconnection
   socket.on('disconnect', function() {
     debug('server disconnected');
   });
+
+  socket.emit('registered as server');
 }
 
-function Client(socket) {
+function Client(socket, name) {
   debug('a client registered');
-  socket.join('clients');
 
-  socket.broadcast.to('servers').emit('client connection', { id: socket.id });
+  var clientRoom = 'clients:' + name;
+  var serverRoom = 'server:' + name;
+
+  socket.join(clientRoom);
+
+  socket.broadcast.to(serverRoom).emit('client connection', { id: socket.id });
 
   // message
   socket.on('message', function(msg) {
     msg.id = socket.id;
-    socket.broadcast.to('servers').emit('message', msg);
+    socket.broadcast.to(serverRoom).emit('message', msg);
   });
 
   // disconnection
   socket.on('disconnect', function() {
     debug('user disconnected');
   });
+
+  socket.emit('registered as client');
 }
 
 function debug() {
   var i;
-  console.log('\n===== DEBUG ======\n');
+  console.log('\n===== DEBUG ======');
   for (i in arguments) {
     console.log(arguments[i]);
   }
+}
+
+function findClientsSocket(roomId, namespace) {
+    var res = []
+    , ns = io.of(namespace ||"/");    // the default namespace is "/"
+
+    if (ns) {
+        for (var id in ns.connected) {
+            if(roomId) {
+                var index = ns.connected[id].rooms.indexOf(roomId) ;
+                if(index !== -1) {
+                    res.push(ns.connected[id]);
+                }
+            } else {
+                res.push(ns.connected[id]);
+            }
+        }
+    }
+    return res;
 }
